b1c1ed76020dc90a0422b6616f40d70e2ddd459b
NizamZuhrana <zams.poenya@gmail.com>
2025-01-30 22:57:43 +0700

    updated homePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,29 +4,23 @@ import Banner from "../../assets/banner-1.jpg";
 import useData from "../../hooks/useData";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-
-// Import user images
-import FootballImage from "../../assets/Football.jpg";
-import FutsalImage from "../../assets/futsal.jpg";
-import MinsocImage from "../../assets/minsoc.jpg";
-import BadmintonImage from "../../assets/badminton.jpg";
-import BasketballImage from "../../assets/basketball.jpg";
-
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const HomePage = () => {
-  const { getUseData, useUsers, ratings } = useData();
+  const {
+    getUseData,
+    useUsers,
+    ratings,
+    pagination,
+    handleNextPage,
+    handlePreviousPage,
+    images,
+  } = useData();
   const { id } = useParams();
-
-  const images = [
-    { id: 1, name: "Football", image: FootballImage },
-    { id: 2, name: "Futsal", image: FutsalImage },
-    { id: 3, name: "Minsoc", image: MinsocImage },
-    { id: 4, name: "Badminton", image: BadmintonImage },
-    { id: 5, name: "Basketball", image: BasketballImage },
-  ];
 
   useEffect(() => {
     getUseData();
-  }, [id]);
+  }, [id, pagination.current_page]);
 
   return (
     <div className="relative">
@@ -53,95 +47,122 @@ const HomePage = () => {
       </div>
 
       {/* Users Section */}
-      <div className="container px-4 mx-auto mt-8">
-        <h3 className="mb-6 text-2xl font-bold text-center">Our Users</h3>
+      <div className="container relative px-4 mx-auto mt-8">
+        <h3 className="text-2xl font-bold px-5 py-2.5 my-2 mx-2">
+          Cabang Olahraga
+        </h3>
+        <Link to="/explore">
+          <button
+            type="button"
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-2 my-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 "
+          >
+            Temukan Lapangan Keinginanmu
+            <span className="inline-flex items-center justify-center w-4 h-4 text-xs font-semibold ms-2 ">
+              <FaSearch />
+            </span>
+          </button>
+        </Link>
+
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {useUsers.map((user) => {
-            // Cari gambar berdasarkan user.id
             const userImage = images.find((img) => img.id === user.id)?.image;
-
             return (
               <div
                 key={user.id}
-                className="p-6 transition-shadow duration-300 bg-white rounded-lg shadow-lg hover:shadow-xl"
+                className="p-6 transition-transform duration-300 bg-white border border-gray-200 rounded-lg shadow-lg hover:shadow-2xl hover:scale-105 hover:border-transparent hover:bg-gradient-to-r hover:from-[#F9F9F9] hover:via-[#FDFDFD] hover:to-[#FFFFFF]"
               >
                 {/* User Image */}
-                <div className="w-full h-48 mb-4 overflow-hidden rounded-lg">
+                <div className="w-full h-48 mb-4 overflow-hidden border-2 border-gray-200 rounded-lg">
                   <img
-                    src={userImage || "../../assets/default.jpg"} // Fallback ke gambar default jika tidak ditemukan
+                    src={userImage || "../../assets/default.jpg"}
                     alt={`${user.name}'s Avatar`}
                     className="object-cover w-full h-full"
                   />
                 </div>
-                <h4 className="text-xl font-semibold text-center text-gray-800">
+                <h4 className="text-xl font-semibold text-center text-gray-800 ">
                   {user.name}
                 </h4>
-                <p className="text-center text-gray-600">
-                  Email: {user.email || "Not Provided"}
-                </p>
-                <p className="text-center text-gray-600">
-                  Username: {user.username || "N/A"}
-                </p>
               </div>
             );
           })}
         </div>
 
-        <div className="container px-4 mx-auto mt-12">
-          <h3 className="mb-6 text-2xl font-bold text-center text-gray-800">
-            User Reviews
-          </h3>
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-            {ratings.map((rating) => (
-              <div
-                key={rating.id}
-                className="relative p-6 bg-gradient-to-r from-[#ECECEC] via-[#F8F8F8] to-[#FFFFFF] rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300"
-              >
-                {/* User Info */}
-                <div className="flex items-center mb-4">
-                  <div className="ml-4">
-                    <h4 className="text-lg font-semibold text-gray-800">
-                      {rating.user.name}
-                    </h4>
-                    <p className="text-sm text-gray-500">
-                      {new Date(rating.timestamp).toLocaleDateString()}
-                    </p>
-                  </div>
+        {/* Pagination Controls */}
+        <div className="flex justify-between mt-8">
+          <button
+            onClick={handlePreviousPage}
+            disabled={pagination.page === 1}
+            className="px-4 py-2 font-semibold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 disabled:opacity-50"
+          >
+            Previous
+          </button>
+          <span className="text-lg font-medium">
+            Page {pagination.current_page} of {pagination.total_pages}
+          </span>
+          <button
+            onClick={handleNextPage}
+            disabled={pagination.page === pagination.total_pages}
+            className="px-4 py-2 font-semibold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 disabled:opacity-50"
+          >
+            Next
+          </button>
+        </div>
+      </div>
+
+      <div className="container px-4 mx-auto mt-12">
+        <h3 className="mb-6 text-2xl font-bold text-center text-gray-800">
+          Ini Rating Mereka kepada Oyok
+        </h3>
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {ratings.map((rating) => (
+            <div
+              key={rating.id}
+              className="relative p-6 bg-gradient-to-r from-[#ECECEC] via-[#F8F8F8] to-[#FFFFFF] rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300"
+            >
+              {/* User Info */}
+              <div className="flex items-center mb-4">
+                <div className="ml-4">
+                  <h4 className="text-lg font-semibold text-gray-800">
+                    {rating.user.name}
+                  </h4>
+                  <p className="text-sm text-gray-500">
+                    {new Date(rating.timestamp).toLocaleDateString()}
+                  </p>
                 </div>
-
-                {/* Star Rating */}
-                <div className="flex items-center mb-2">
-                  {Array.from({ length: 5 }).map((_, index) => (
-                    <svg
-                      key={index}
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill={index < Math.round(rating.rating) ? "gold" : "none"}
-                      stroke="gold"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
-                      />
-                    </svg>
-                  ))}
-                  <span className="ml-2 text-lg font-semibold text-gray-800">
-                    {rating.rating.toFixed(1)}
-                  </span>
-                </div>
-
-                {/* Comment */}
-                <p className="italic leading-relaxed text-gray-700">
-                  “{rating.comment}”
-                </p>
-
-                {/* Highlight Border */}
-                <div className="absolute inset-0 transition-colors duration-300 border-2 border-transparent rounded-xl hover:border-yellow-400"></div>
               </div>
-            ))}
-          </div>
+
+              {/* Star Rating */}
+              <div className="flex items-center mb-2">
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <svg
+                    key={index}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill={index < Math.round(rating.rating) ? "gold" : "none"}
+                    stroke="gold"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
+                    />
+                  </svg>
+                ))}
+                <span className="ml-2 text-lg font-semibold text-gray-800">
+                  {rating.rating.toFixed(1)}
+                </span>
+              </div>
+
+              {/* Comment */}
+              <p className="italic leading-relaxed text-gray-700">
+                “{rating.comment}”
+              </p>
+
+              {/* Highlight Border */}
+              <div className="absolute inset-0 transition-colors duration-300 border-2 border-transparent rounded-xl hover:border-yellow-400"></div>
+            </div>
+          ))}
         </div>
       </div>
       <Footer />
