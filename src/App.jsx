dcf40d5716760888d20b3e70328ce855a75c350a
NizamZuhrana <zams.poenya@gmail.com>
2025-02-07 20:50:25 +0700

    updated tailwind.config and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import DetailPage from "./pages/ExploreDetailPage";
-import SearchFilter from "./components/SearchFilters";
 import DashboardAdmin from "./pages/DashboardPage";
 import MyTransaction from "./pages/My-TransactionPage";
 import MyTransactionID from "./pages/My-TransactionPage/[id]";
 import ProtectRoutes from "./routes/protectRoutes";
+import Explore from "./pages/ExplorePage";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/explore" element={<SearchFilter />} />
+        <Route path="/explore" element={<Explore />} />
 
         {/* Explore by ID hanya untuk user */}
         <Route
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
 const flowbite = require("flowbite-react/tailwind");
 export default {
   content: [
     "./index.html",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
     flowbite.content(),
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "background-1" : 'url("./src/assets/backgrond-1.jpg")',
-        "logo-1" : 'url("./src/assets/astronot.jpeg")',
-      }
+        "background-1": 'url("./src/assets/backgrond-1.jpg")',
+        "logo-1": 'url("./src/assets/astronot.jpeg")',
+        "background-2": 'url("./src/assets/football3.jpg")',
+        "background-3": 'url("./src/assets/poster-4.jpeg")',
+      },
+      fontFamily: {
+        "open-sans": ["Open Sans", "sans-serif"],
+        "roboto-slab": ["Roboto Slab", "serif"],
+      },
     },
   },
-  plugins: [
-    flowbite.plugin(),
-  ],
-}
-
+  plugins: [flowbite.plugin()],
+};
