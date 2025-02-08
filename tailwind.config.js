03d8c770fa361107b0184385150e4a0f9b0974d4
NizamZuhrana <zams.poenya@gmail.com>
2025-02-08 13:59:33 +0700

    updated images and tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,10 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "background-1": 'url("./src/assets/backgrond-1.jpg")',
+        "background-1": 'url("/backgrond-1.jpg")',
         "logo-1": 'url("./src/assets/astronot.jpeg")',
-        "background-2": 'url("./src/assets/football3.jpg")',
-        "background-3": 'url("./src/assets/poster-4.jpeg")',
+        "background-2": 'url("/football3.jpg")',
+        "background-3": 'url("/poster-4.jpeg")',
       },
       fontFamily: {
         "open-sans": ["Open Sans", "sans-serif"],
